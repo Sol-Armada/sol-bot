ad9c416ac794b0d71865ebe4c811b0720284acdc
Douglas DeVicariis <douglas@devicariis.org>
2023-03-03 20:24:41 -0500

    onboarding fix

--- bot/handlers/onboarding.go
+++ bot/handlers/onboarding.go
@@ -551,7 +551,7 @@ func finish(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
 	go func() {
 		for i := 29; i > 0; i-- {
-			time.Sleep(1 * time.Second)
+			time.Sleep(1 * time.Minute)
 			if _, err := s.ChannelMessageEdit(
 				message.ChannelID,
 				message.ID,
